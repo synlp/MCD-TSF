9b96571ab8aeb24898e21b0449a97d896637c93c
Deepiin <suchen4565@163.com>
2025-04-28 15:12:22 +0800

    update

---
+++ run.sh
@@ -0,0 +1,206 @@
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Traffic/Traffic.csv \
+    --config traffic_36_6.yaml \
+    --seq_len 36 \
+    --pred_len 6 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Traffic/Traffic.csv \
+    --config traffic_36_12.yaml \
+    --seq_len 36 \
+    --pred_len 12 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Traffic/Traffic.csv \
+    --config traffic_36_18.yaml \
+    --seq_len 36 \
+    --pred_len 18 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path SocialGood/SocialGood.csv \
+    --config socialgood_36_6.yaml \
+    --seq_len 36 \
+    --pred_len 6 \
+    --freq m
+ 
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path SocialGood/SocialGood.csv \
+    --config socialgood_36_12.yaml \
+    --seq_len 36 \
+    --pred_len 12 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path SocialGood/SocialGood.csv \
+    --config socialgood_36_18.yaml \
+    --seq_len 36 \
+    --pred_len 18 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Health_US/Health_US.csv \
+    --config health_96_12.yaml \
+    --seq_len 96 \
+    --pred_len 12 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Health_US/Health_US.csv \
+    --config health_96_24.yaml \
+    --seq_len 96 \
+    --pred_len 24 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Health_US/Health_US.csv \
+    --config health_96_48.yaml \
+    --seq_len 96 \
+    --pred_len 48 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Environment/Environment.csv \
+    --config environment_336_48.yaml \
+    --seq_len 336 \
+    --pred_len 48 \
+    --seed 2021 \
+    --freq d
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Environment/Environment.csv \
+    --config environment_336_96.yaml \
+    --seq_len 336 \
+    --pred_len 96 \
+    --seed 2021 \
+    --freq d
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Environment/Environment.csv \
+    --config environment_336_192.yaml \
+    --seq_len 336 \
+    --pred_len 192 \
+    --seed 2021 \
+    --freq d
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Energy/Energy.csv \
+    --config energy_96_12.yaml \
+    --seq_len 96 \
+    --pred_len 12 \
+    --text_len 36 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Energy/Energy.csv \
+    --config energy_96_24.yaml \
+    --seq_len 96 \
+    --pred_len 24 \
+    --text_len 36 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Energy/Energy.csv \
+    --config energy_96_48.yaml \
+    --seq_len 96 \
+    --pred_len 48 \
+    --text_len 36 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Economy/Economy.csv \
+    --config economy_36_6.yaml \
+    --seq_len 36 \
+    --pred_len 6 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Economy/Economy.csv \
+    --config economy_36_12.yaml \
+    --seq_len 36 \
+    --pred_len 12 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Economy/Economy.csv \
+    --config economy_36_18.yaml \
+    --seq_len 36 \
+    --pred_len 18 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Climate/Climate.csv \
+    --config climate_96_12.yaml \
+    --seq_len 96 \
+    --pred_len 12 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Climate/Climate.csv \
+    --config climate_96_24.yaml \
+    --seq_len 96 \
+    --pred_len 24 \
+    --freq w
+
+python -u exe_forecasting.py \
+    --root_path ../Time-MMD-main \
+    --data_path Climate/Climate.csv \
+    --config climate_96_48.yaml \
+    --seq_len 96 \
+    --pred_len 48 \
+    --freq w
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Agriculture/Agriculture.csv \
+    --config agriculture_36_6.yaml \
+    --seq_len 36 \
+    --pred_len 6 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Agriculture/Agriculture.csv \
+    --config agriculture_36_12.yaml \
+    --seq_len 36 \
+    --pred_len 12 \
+    --text_len 36 \
+    --freq m
+
+python -u exe_forecasting.py\
+    --root_path ../Time-MMD-main \
+    --data_path Agriculture/Agriculture.csv \
+    --config agriculture_36_18.yaml \
+    --seq_len 36 \
+    --pred_len 18 \
+    --text_len 36 \
+    --freq m
